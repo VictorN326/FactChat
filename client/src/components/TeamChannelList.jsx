b7d94da978200c9bf0d630d39d4601290eee74a1
VictorN326 <21vnguyen3@gmail.com>
2023-03-27 01:26:38 -0700

    Worked On the Channel Components. Needs more work

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import { AddChannel } from '../assets';
-const TeamChannelList = ({}) => {
+const TeamChannelList = ({children, error = false, loading, type}) => {
+  if(error) {
+    return type === 'team' ? (
+      <div className='team-channel-list'>
+        <p className='team-channel-list__message'>
+          Connection error, please wait a moment and try again.
+        </p>
+      </div>
+    ): null
+  };
+  if (loading) {
+    return (
+      <div className='team-channel-list'>
+        <p className='team-channel-list__message loading'>
+          {type === 'team' ? 'Channels': 'Messages'} Loading...
+        </p>
+      </div>
+    )
+  }
   return (
-    <div>TeamChannelList</div>
+    <div className='team-channel-list'>
+      <div className='team-channel-list__header'>
+        <p className='team-channel-list__header__title'>
+          {type === 'team' ? 'Channels': 'Direct Messages'}
+        </p>
+        //TODO: Button goes here to add channels
+      </div>
+      {children}
+    </div>
   )
 }
 
