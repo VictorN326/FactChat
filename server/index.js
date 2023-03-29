bbc4b807a1109da83c5b5ccad6f9e528d8a6c7ff
VictorN326 <21vnguyen3@gmail.com>
2023-03-29 03:24:22 -0700

    Worked On backend logic

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+
+// Load the dotenv configuration
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Use middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/auth", authRoutes);
+
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
